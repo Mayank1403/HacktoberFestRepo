0b5d086819de701123399b499d1d73d579ca1649
Amritesh <amriteshc101@icloud.com>
2021-10-09 23:08:10 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -20,22 +20,22 @@ print("4.Divide")
 
 while True:
     
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(+/-/*//): ")
 
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
-        if choice == '1':
+        if choice == '+':
             print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '2':
+        elif choice == '-':
             print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
+        elif choice == '*':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '4':
+        elif choice == '/':
             print(num1, "/", num2, "=", divide(num1, num2))
         
         
