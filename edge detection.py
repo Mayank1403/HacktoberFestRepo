8ca8fe3ab0a974426d7265e8b8c0a24a090093f4
Ujwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2021-10-26 12:28:29 +0530

    Code is updated & added more fuction
    Code is updated & added more fuction

--- edge detection.py
+++ edge detection.py
@@ -1,43 +1,38 @@
-# IMAGE GRADIENT: directional change in the intensity/color in an image
-# This is one the fundamental builiding block in image processing.
-# Image gradient is used to find edges inside the image.
-
+# Python program to Edge detection
+# using OpenCV in Python
+# using Sobel edge detection
+# and laplacian method
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
-# IMAGE Gradient methods:
-# 1.) Laplacian derivatives
-# 2.) Sobal X              # this is joint gaussian and differentiation operations.
-# 3.) Sobal Y              # Horizontal changes observed properly.
-# 4.) Sobel combined
-# 5.) Canny edge detection
-# Canny: edge detection operator that uses multi stage algo to detect wide range of edges.
-# Canny: (Noise reduction+gradient calculation+non-max suppression+double threshold+edge tracking by Hysteresis)
-# these methods uses different mathematical operations to produce the required image.
+#Capture livestream video content from camera 0
+cap = cv2.VideoCapture(0)
 
-img = cv2.imread('selfie.JPG',cv2.IMREAD_GRAYSCALE)
-lap = cv2.Laplacian(img,cv2.CV_64F,ksize=3)
-# CV_64F is just a data-type, 64 bit float that supports negative number
-lap = np.uint8(np.absolute(lap))
-# this output is suitable for our work, absolute work to unsigned 8 bit integer.
-sobelX = cv2.Sobel(img,cv2.CV_64F,1,0)
-# 3rd argument is 1(order of derivative in x dir), 4th is 0 for dy in case of sobelX.
-sobelY = cv2.Sobel(img,cv2.CV_64F,0,1)
-# 3rd argument is 0, 4th(order of derivative in y dir) is 1 for dy in case of sobelX.
-sobelX = np.uint8(np.absolute(sobelX))
-sobelY = np.uint8(np.absolute(sobelY))
+while(1):
 
-sobelcombine = cv2.bitwise_or(sobelX,sobelY)
+	# Take each frame
+	_, frame = cap.read()
+	
+	# Convert to HSV for simpler calculations
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	
+	# Calculation of Sobelx
+	sobelx = cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5)
+	
+	# Calculation of Sobely
+	sobely = cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=5)
+	
+	# Calculation of Laplacian
+	laplacian = cv2.Laplacian(frame,cv2.CV_64F)
+	
+	cv2.imshow('sobelx',sobelx)
+	cv2.imshow('sobely',sobely)
+	cv2.imshow('laplacian',laplacian)
+	k = cv2.waitKey(5) & 0xFF
+	if k == 27:
+		break
 
-edges = cv2.Canny(img,100,200)
+cv2.destroyAllWindows()
 
-titles = ['Image','Laplacian','SobelX','SobelY','Sobel combined','Canny']
-images = [img,lap,sobelX,sobelY,sobelcombine,edges]
-
-for i in range(6):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-
-plt.show()
+#release the frame
+cap.release()
