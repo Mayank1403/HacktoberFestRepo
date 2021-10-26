0862f1bda90ab9aa7c7be1c5b2d38e03a0e2a61e
Ujwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2021-10-26 12:24:49 +0530

    Additional changes
    Additional changes added comment and fuction

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,11 +1,11 @@
 #This is a hello Wold program
 #hello 
 
-print("Hello Print")
+print("Hello Hello") //it is print function to use print statement
 print("changes")
-print("This is another print")
+print("This is another print statement")
 print("Testing another branch pull request!!")
-for i in range(10):
-  print("This is Hacktober Fest 2021")
+for i in range(1,11):
+  print("This is Hacktober Fest Festival 2021")
 
 print("thanks")
